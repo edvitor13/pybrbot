4c508a447a1dd5ef1997f260cefb965ab4fc2cff
edvitor13 <edvitor13@hotmail.com>
2021-09-21 22:40:40 -0300

    Creation of main function

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import discord
 from src.PyBrBot import Bot, Config
 
-# Solicitando modo do BOT
-test_mode = input("Ativar modo teste? (S/n) ")
+def main():
+    # Solicitando modo do BOT
+    test_mode = input("Ativar modo teste? (S/n) ")
 
-# Ativa o modo oficial caso ativação seja negada
-if (test_mode.lower() in ["n", "nao", "não"]):
-    test_mode = False
-    token = Config.get('token')
-    command_prefix = Config.get('bot_commands_prefix')
-else:
-    test_mode = True
-    token = Config.get('test_token')
-    command_prefix = Config.get('test_bot_commands_prefix')
+    # Ativa o modo oficial caso ativação seja negada
+    if (test_mode.lower() in ["n", "nao", "não"]):
+        test_mode = False
+        token = Config.get('token')
+        command_prefix = Config.get('bot_commands_prefix')
+    else:
+        test_mode = True
+        token = Config.get('test_token')
+        command_prefix = Config.get('test_bot_commands_prefix')
 
-# Inciando BOT
-if (__name__ == "__main__"):
     # Alterando intents
     intents = discord.Intents.default()
     intents.members = True
-    
+    intents.messages = True
+
+    # Instanciando Bot
     client = Bot(
         intents=intents,
         command_prefix=command_prefix, 
         remove_commands=['help']
     )
+
+    # Rodando
     client.run(token)
+
+# Iniciando Bot
+if (__name__ == "__main__"):
+    main()
